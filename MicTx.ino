0dd02d4c9d117d3b5cfedce53d6d6e456ca889ed
Jacob Armstrong <43141265+jakeparmstrong@users.noreply.github.com>
2019-11-12 14:24:41 -0500

    Create MicTx.ino

---
+++ MicTx.ino
@@ -0,0 +1,108 @@
+
+
+// Clean this up
+// This script will be on the nano that reads from a contact mic and sends the data over NRF module
+// also could add temp sensor and clamp both to sax neck?
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+#define CE_PIN   9
+#define CSN_PIN 10
+
+const byte slaveAddress[5] = {'R','x','A','A','A'};
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+ 
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+
+char dataToSend[10] = "Message 0";
+char txNum = '0';
+
+
+unsigned long currentMillis;
+unsigned long prevMillis;
+unsigned long txIntervalMillis = 1000; // send once per second
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+    Serial.println("SimpleTx Starting");
+
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.setRetries(3,5); // delay, count
+    radio.openWritingPipe(slaveAddress);
+}
+
+//====================
+
+void loop() {
+    currentMillis = millis();
+    
+    if (currentMillis - prevMillis >= txIntervalMillis) {
+        send();
+        prevMillis = millis();
+    }
+}
+
+//====================
+
+void send() {
+  unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+   Serial.println(peakToPeak);
+    bool rslt;
+    rslt = radio.write( &peakToPeak, sizeof(peakToPeak) );
+        // Always use sizeof() as it gives the size as the number of bytes.
+        // For example if dataToSend was an int sizeof() would correctly return 2
+
+    Serial.print("Data Sent ");
+    Serial.print(dataToSend);
+    if (rslt) {
+        Serial.println("  Acknowledge received");
+        updateMessage();
+    }
+    else {
+        Serial.println("  Tx failed");
+    }
+}
+
+//================
+
+void updateMessage() {
+        // so you can see that new data is being sent
+    txNum += 1;
+    if (txNum > '9') {
+        txNum = '0';
+    }
+    dataToSend[8] = txNum;
+}
